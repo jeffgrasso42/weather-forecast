63fa95ee0985f024744e12ad62ad0d9e3628d6d0
JAG <jagrasso42@gmail.com>
2022-10-11 21:09:13 -0400

    added variables and dependencies to assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 // ASSIGNMENT CODE
 // variable for the API key
+var weatherKey = '66b15a5b3951d15de56c5d2c4e2ddcba';
 // variable for the lattitude and longitude
+var latLon = {};
 
 
 // DEPENDENCIES
 // variable for the button elements' container element
-// variable for the weather section element
+var searchCardEl = document.getElementById('search-card');
+// variable for the current weather container
+var todayContainerEl = document.querySelector('.today-container');
 // variables for the 5-day forecast cards
+var day1El = document.getElementById('day-1');
+var day2El = document.getElementById('day-2');
+var day3El = document.getElementById('day-3');
+var day4El = document.getElementById('day-4');
+var day5El = document.getElementById('day-5');
 
 // FUNCTIONS
 // init function that displays default city weather forecast
