3052e89d34c2b40479393ae3010ad400486cb130
JAG <jagrasso42@gmail.com>
2022-10-11 21:06:45 -0400

    updated pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 // ASSIGNMENT CODE
-  // variable for the API key and 
-  // variable for the lattitude and longitude
+// variable for the API key
+// variable for the lattitude and longitude
+
 
 // DEPENDENCIES
-  // variable for the button elements' container element
-  // variable for the weather section element
-  // variables for the 5-day forecast cards
+// variable for the button elements' container element
+// variable for the weather section element
+// variables for the 5-day forecast cards
 
 // FUNCTIONS
-// Get Weather Function with cityName parameter
+// init function that displays default city weather forecast
+
+// Retrieve search history from local storage on load
+
+// Store searched city in local storage
 
 // checkSelection function that handles user input
 
-// init function that displays default city weather forecast
+// Get Weather Function with cityName parameter
+
+// Render data from API on page
 
 // INITIALIZATION
 
 // EVENT LISTENERS
-  // click event listener on the search-card element
+// click event listener on the search-card element
 
