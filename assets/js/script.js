076a2c5182f43e235a0ff075db397ad859ec16f6
JAG <jagrasso42@gmail.com>
2022-10-11 20:55:01 -0400

    pseudocode added to  assets/js/script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// ASSIGNMENT CODE
+  // variable for the API key and 
+  // variable for the lattitude and longitude
+
+// DEPENDENCIES
+  // variable for the button elements' container element
+  // variable for the weather section element
+  // variables for the 5-day forecast cards
+
+// FUNCTIONS
+// Get Weather Function with cityName parameter
+
+// checkSelection function that handles user input
+
+// init function that displays default city weather forecast
+
+// INITIALIZATION
+
+// EVENT LISTENERS
+  // click event listener on the search-card element
+
